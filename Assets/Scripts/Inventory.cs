2552121e73999198abd0c9edba242032c1de6bce
Hasan Eroglu <hasanheroglu@gmail.com>
2020-12-28 20:57:09 +0300

    Initalize the project.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    private int _width;
+    [SerializeField]
+    private int _height;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/InventoryGrid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryGrid : MonoBehaviour
+{
+    [SerializeField]
+    private int _width;
+    [SerializeField]
+    private int _height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
